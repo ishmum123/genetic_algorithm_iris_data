74a81adb2e6c467e7e8f678140126cfb9c8da932
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2017-07-24 02:10:39 +0600

    1 & 2 Argument Function Added

--- function_generator.py
+++ function_generator.py
@@ -1,10 +1,57 @@
+from math import exp, log, log10, log1p, sqrt, sin, cos, pow, fmod
+
+
 class FunctionGenerator:
-	def getFunction(self, num_arg):
+	def getFunction(self, num_arg, func_num):
 		func = None
+		def m_rnd(x):
+			return round(x, 2)
 		if num_arg == 1: 
-			func = lambda x: x + 1
+			if func_num == 0:
+				func = lambda x: x + 1
+			elif func_num == 1:
+				func = lambda x: m_rnd(sqrt(x))
+			elif func_num == 2:
+				func = lambda x: m_rnd(exp(x))
+			elif func_num == 3:
+				func = lambda x: m_rnd(exp(-x))
+			elif func_num == 4:
+				func = lambda x: m_rnd(log10(x))
+			elif func_num == 5:
+				func = lambda x: m_rnd(log1p(x))
+			elif func_num == 6:
+				func = lambda x: m_rnd(cos(x))
+			elif func_num == 7:
+				func = lambda x: m_rnd(sin(x))
+			elif func_num == 8:
+				func = lambda x: m_rnd(pow(x, 2))
+			else:
+				func = lambda x: x
 		elif num_arg == 2:
-			func = lambda x, y: x + y
+			if func_num == 0:
+				func = lambda x, y: x + y
+			elif func_num == 1:
+				func = lambda x, y: x - y
+			elif func_num == 2:
+				func = lambda x, y: m_rnd(x * y)
+			elif func_num == 3:
+				func = lambda x, y: m_rnd(x / y)
+			elif func_num == 4:
+				func = lambda x, y: m_rnd(pow(x, y))
+			elif func_num == 5:
+				func = lambda x, y: m_rnd(pow(y, x))
+			elif func_num == 6:
+				func = lambda x, y: m_rnd(log(y, x))
+			elif func_num == 7:
+				func = lambda x, y: m_rnd(log(x, y))
+			elif func_num == 8:
+				func = lambda x, y: m_rnd(fmod(y, x))
+			elif func_num == 9:
+				func = lambda x, y: m_rnd(fmod(x, y))
+			elif func_num == 10:
+				func = lambda x, y: y
+			else:
+				func = lambda x, y: x
 		else:
 			raise ValueError
 		return func
--- test_function_generator.py
+++ test_function_generator.py
@@ -6,19 +6,24 @@ class TestFunctionGenerator(TestCase):
 	def setUp(self):
 		self.generator = FunctionGenerator()
 
-	def test_getFunction_returns_function(self):
-		self.assertTrue(callable(self.generator.getFunction(1)))
+	def test_getFunction_returns_function_taking_two_arguments(self):
+		test_func = self.generator.getFunction
+		self.assertTrue(callable(test_func(1, 1)))
+		with self.assertRaises(TypeError):
+			test_func()
+		with self.assertRaises(TypeError):
+			test_func(1)
 
 	def test_getFunction_returns_1_argument_function_given_1(self):
-		test_func = self.generator.getFunction(1)
-		with self.assertRaises(Exception):
+		test_func = self.generator.getFunction(1, 0)
+		with self.assertRaises(TypeError):
 			test_func()
-		with self.assertRaises(Exception):
+		with self.assertRaises(TypeError):
 			test_func(0, 0)
 		test_func(0)
 
 	def test_getFunction_returns_2_argument_function_given_2(self):
-		test_func = self.generator.getFunction(2)
+		test_func = self.generator.getFunction(2, 0)
 		with self.assertRaises(TypeError):
 			test_func()
 		with self.assertRaises(TypeError):
@@ -26,9 +31,15 @@ class TestFunctionGenerator(TestCase):
 		test_func(0, 0)
 
 	def test_getFunction_raises_ValuEerror_if_not_supplied_with_1_or_2(self):
+		test_func = self.generator.getFunction
 		with self.assertRaises(ValueError):
-			self.generator.getFunction(0)
+			test_func(0, 0)
 		with self.assertRaises(ValueError):
-			self.generator.getFunction(3)
-		with self.assertRaises(Exception):
-			self.generator.getFunction("anything")
+			test_func(3, 0)
+		with self.assertRaises(ValueError):
+			test_func("anything", 0)
+
+	def test_getFunction_returns_different_functions_for_some_func_num(self):
+		test_func = self.generator.getFunction
+		self.assertNotEqual(test_func(1, 0)(0), test_func(1, 1)(0))
+		self.assertNotEqual(test_func(2, 0)(0, 1), test_func(2, 1)(0, 1))

9f5570af622f3fe67ee606458d064f6a405817aa
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2017-07-24 02:24:06 +0600

    Made Use of inspect.signature for Function Argument Test

--- test_function_generator.py
+++ test_function_generator.py
@@ -1,34 +1,27 @@
 from unittest import TestCase
 from function_generator import FunctionGenerator
+from inspect import signature
 
 
 class TestFunctionGenerator(TestCase):
 	def setUp(self):
 		self.generator = FunctionGenerator()
 
+	def getFunctionLength(self, function):
+		return len(signature(function).parameters)
+
 	def test_getFunction_returns_function_taking_two_arguments(self):
 		test_func = self.generator.getFunction
 		self.assertTrue(callable(test_func(1, 1)))
-		with self.assertRaises(TypeError):
-			test_func()
-		with self.assertRaises(TypeError):
-			test_func(1)
+		self.assertEqual(self.getFunctionLength(test_func), 2)
 
 	def test_getFunction_returns_1_argument_function_given_1(self):
 		test_func = self.generator.getFunction(1, 0)
-		with self.assertRaises(TypeError):
-			test_func()
-		with self.assertRaises(TypeError):
-			test_func(0, 0)
-		test_func(0)
+		self.assertEqual(self.getFunctionLength(test_func), 1)
 
 	def test_getFunction_returns_2_argument_function_given_2(self):
 		test_func = self.generator.getFunction(2, 0)
-		with self.assertRaises(TypeError):
-			test_func()
-		with self.assertRaises(TypeError):
-			test_func(0)
-		test_func(0, 0)
+		self.assertEqual(self.getFunctionLength(test_func), 2)
 
 	def test_getFunction_raises_ValuEerror_if_not_supplied_with_1_or_2(self):
 		test_func = self.generator.getFunction
